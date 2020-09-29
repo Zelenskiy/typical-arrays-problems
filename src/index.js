a6c93d5465729ac21daac77f82e5a0e556b028d0
Zelenskiyas <zelensk1973@gmail.com>
2020-09-29 15:46:42 +0300

    feat: add avg function

--- src/index.js
+++ src/index.js
@@ -20,5 +20,10 @@ exports.max = function max (array) {
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let m = 0;
+  for (let i=0; i<array.length; i++){
+    m += array[i];    
+  }
+  m /= array.length;
+  return m;
 }
