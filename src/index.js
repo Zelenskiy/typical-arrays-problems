839469206a7b47b8dcdbb0fff67278919ca3c336
Zelenskiyas <zelensk1973@gmail.com>
2020-09-29 15:41:52 +0300

    feat: add min function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 
 exports.min = function min (array) {
-  return min(array);
+  let m = array[0];
+  for (let i=0; i<array.length; i++){
+    if (array[i] < m) {
+      m = array[i];
+    }
+  }
+  return m;
 }
 
 exports.max = function max (array) {
